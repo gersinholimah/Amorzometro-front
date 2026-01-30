fa7088998f9c8f51a1bbf66fd51a9dcc4718ae7b
gslima <gerson.lima@detran.ba.gov.br>
2026-01-30 17:43:50 -0300

    [FEATURE] Cria estrutura de tela para pagamentos

---
+++ src/app/modules/admin/pages/pagamentos/pagamentos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pagamentos',
+  imports: [],
+  templateUrl: './pagamentos.html',
+  styleUrl: './pagamentos.css',
+})
+export class Pagamentos {
+
+}
