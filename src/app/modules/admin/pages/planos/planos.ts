eba7c4fae64f4031a3197d3d3ba6f77be7582b66
gslima <gerson.lima@detran.ba.gov.br>
2026-01-30 17:44:09 -0300

    [FEATURE] Cria estrutura de tela para planos

---
+++ src/app/modules/admin/pages/planos/planos.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planos',
+  imports: [],
+  templateUrl: './planos.html',
+  styleUrl: './planos.css',
+})
+export class Planos {
+
+}
