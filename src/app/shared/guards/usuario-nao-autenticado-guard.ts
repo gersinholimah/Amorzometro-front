c84dea13d4963e5a316b0b96ec40c3ee442aed79
gslima <gerson.lima@detran.ba.gov.br>
2026-01-31 10:25:51 -0300

    [FIX] Modifica guard de usuario nao autenticado

--- src/app/shared/guards/usuario-nao-autenticado-guard.ts
+++ src/app/shared/guards/usuario-nao-autenticado-guard.ts
@@ -1,19 +1,23 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
-import { IDadosDaSessao } from '../interfaces/estrutura.interface';
 import { GlobalService } from '../service/global.service';
-import { STATUS } from '../constants/routes.constant';
+import { PATH_MODULO, ROLES, STATUS } from '../constants/routes.constant';
 
-export const usuarioNaoAutenticadoGuard: CanActivateFn = (route, state) => {
+export const usuarioNaoAutenticadoGuard: CanActivateFn = () => {
   const router = inject(Router);
   const globalService = inject(GlobalService);
 
-  const dadosDaSessao: IDadosDaSessao | null = globalService.getDadosDaSessao();
+  const dadosDaSessao = globalService.getDadosDaSessao();
   const token = dadosDaSessao?.token;
 
-  if (token) {
-    return router.createUrlTree([STATUS.AUTENTICADO]);
+  if (!token) {
+    return true; // lead pode acessar
   }
 
-  return true;
+  // usuário logado → redireciona pelo perfil
+  if (dadosDaSessao.role === ROLES.ADMIN) {
+    return router.createUrlTree([STATUS.AUTENTICADO, PATH_MODULO.ADMIN]);
+  }
+
+  return router.createUrlTree([STATUS.AUTENTICADO, PATH_MODULO.CLIENTE]);
 };
