a7dfa176a2b80cd171b48ba65ac8819376896a68
gslima <gerson.lima@detran.ba.gov.br>
2026-01-31 13:39:18 -0300

    [FIX] Corrige Problema de rota

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,47 +1,41 @@
+import { Login } from './modules/acesso/pages/login/login';
 
 
 
 import { usuarioNaoAutenticadoGuard } from './shared/guards/usuario-nao-autenticado-guard';
 import { usuarioAutenticadoGuard } from './shared/guards/usuario-autenticado-guard';
 import { PATH_MODULO, ROLES, ROTAS, STATUS } from './shared/constants/routes.constant';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { CriarPagina } from './modules/compra/pages/criar-pagina/criar-pagina';
 import { Pagamento } from './modules/compra/pages/pagamento/pagamento';
 import { roleGuard } from './shared/guards/role-guard';
 import { ACESSO_ROUTES } from './modules/acesso/acesso.routes';
+import { Home } from './modules/home/pages/home/home';
 
 
 export const routes: Routes = [
 
   //  PÚBLICAS
+// { path: '', pathMatch: 'full', redirectTo: PATH_MODULO.HOME},
+{ path: '', component: Home},
+{ path: 'home', component: Home},
 {
-  path: '',
-  redirectTo: PATH_MODULO.HOME,
-  pathMatch: 'full'
+  path: PATH_MODULO.ADMIN, loadComponent: () =>
+    import('./modules/admin/pages/dashboard/dashboard').then(m => m.Dashboard)
 },
 {
-  path: PATH_MODULO.HOME,
-  loadComponent: () =>
-    import('./modules/home/pages/home/home').then(m => m.Home)
-},
-  {
     path: PATH_MODULO.COMPRA,
-    children: [
-      {
+
         loadChildren: () =>//loadComponent
           import('./modules/compra/compra.routes').then(m => m.COMPRA_ROUTES),
-      }
-    ]
+
   },
 
   {
     path: PATH_MODULO.ACESSO,
     canActivate: [usuarioNaoAutenticadoGuard],
-    children: [
-      {
         loadChildren: () =>
           import('./modules/acesso/acesso.routes').then(m => m.ACESSO_ROUTES)
-      },]
   },
 
   //AUTENTICADAS
@@ -68,7 +62,7 @@ export const routes: Routes = [
         data: { roles: [ROLES.ADMIN] }
       },
     ],
-  },
+  },/**/
 
   { path: '**', redirectTo: '' }
     // { path: '**', redirectTo: PATH_MODULO.ACESSO },
--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,20 @@ import { App } from './app/app';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
+
+
+
+/*import { provideRouter } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
+import { routeConfig } from './app/routes';
+import { routes } from './app/app.routes';
+
+bootstrapApplication( App, {
+  providers: [
+     provideRouter(routes)
+  ]
+})
+  .catch((err) => console.error(err));
+*/
