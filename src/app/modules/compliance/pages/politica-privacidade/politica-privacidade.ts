bc086860b198f587df2a95f1902e0af2f1fec7a3
gslima <gerson.lima@detran.ba.gov.br>
2026-01-31 10:19:47 -0300

    [FEATURE] Cria poli priva

---
+++ src/app/modules/compliance/pages/politica-privacidade/politica-privacidade.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-politica-privacidade',
+  standalone: true,
+  imports: [],
+  templateUrl: './politica-privacidade.html',
+  styleUrl: './politica-privacidade.css',
+})
+export class PoliticaPrivacidade {
+
+}
