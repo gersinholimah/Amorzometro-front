8402f393d7499ae6c8aaa94e11a9d1d1cbdabea7
gslima <gerson.lima@detran.ba.gov.br>
2026-02-01 09:45:13 -0300

    [FEATURE] Implementa estrutura de login

--- src/app/modules/acesso/pages/login/login.ts
+++ src/app/modules/acesso/pages/login/login.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrl: './login.css'
 })
 export class Login {
 
+  loginForm!: FormGroup;
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required],
+      lembrar: [false]
+    });
+  }
+
+  togglePassword() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  submit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    }
+  }
 }
