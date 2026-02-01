f2101b6fda1721d2582fb73e650e7e9045e9f0c3
gslima <gerson.lima@detran.ba.gov.br>
2026-02-01 09:46:12 -0300

    [FEATURE] Implementa estrutura da tela criar página

--- src/app/modules/compra/pages/criar-pagina/criar-pagina.ts
+++ src/app/modules/compra/pages/criar-pagina/criar-pagina.ts
@@ -1,9 +1,25 @@
+import { MatCardModule } from '@angular/material/card';
 import { Component } from '@angular/core';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-criar-pagina',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule
+  ],
   templateUrl: './criar-pagina.html',
   styleUrl: './criar-pagina.css',
 })
